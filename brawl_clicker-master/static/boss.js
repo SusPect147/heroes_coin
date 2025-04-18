db74f559949f8991b487fa473c9fc51d2ee5080e
Max27347 <artemmaster900@gmail.com>
2025-04-19 01:05:48 +0300

    игра 4

--- brawl_clicker-master/static/boss.js
+++ brawl_clicker-master/static/boss.js
@@ -1513,7 +1513,7 @@ if (!gameContainer4 || !ball || !survivalTimeElement || !gameOverScreen4 || !fin
         }
 
         if (survivalTime % 10 === 0 && survivalTime > 0) {
-            spawnInterval = Math.max(500, spawnInterval - 200);
+            spawnInterval = Math.max(500, spawnInterval - 100);
             obstacleSpeed += 0.35;
             console.log("New spawn interval:", spawnInterval, "New obstacle speed:", obstacleSpeed);
         }
